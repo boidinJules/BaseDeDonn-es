a70a9c85ac875544ca203768f0740dbdb9169b1d
boidinJules <144659808+boidinJules@users.noreply.github.com>
2024-11-25 16:58:52 +0000

    triangle en fonction de l'url

--- code/triangle.php
+++ code/triangle.php
@@ -1,11 +1,15 @@
 <?php
 function triangle(){
+    $nb_deligne = $_GET['n'];
     $etoile = "";
-   for($i = 0;$i < 10;$i++){
+    
+   for($i = 0;$i < $nb_deligne; $i++){
         $etoile .= "*";
-        echo $etoile;
-        echo "\n";
+        echo $etoile . "<br>";
     }
 }
 return triangle();
+
+// 127.0.0.1:8081/triangle.php?n=10
+
 ?>
